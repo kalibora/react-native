2a7c44a0bb7c1ded90acbc1b1c817d01c37effcb
Dulmandakh <dulmandakh@gmail.com>
2018-08-10 01:48:49 +0800

    SwitchView depends on AppCompat

--- ReactAndroid/src/main/java/com/facebook/react/views/switchview/ReactSwitchManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/switchview/ReactSwitchManager.java
@@ -20,7 +20,6 @@ import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.UIManagerModule;
 import com.facebook.react.uimanager.ViewProps;
 import com.facebook.react.uimanager.annotations.ReactProp;
-import com.facebook.react.uimanager.events.EventDispatcher;
 import com.facebook.yoga.YogaMeasureFunction;
 import com.facebook.yoga.YogaMeasureMode;
 import com.facebook.yoga.YogaMeasureOutput;
